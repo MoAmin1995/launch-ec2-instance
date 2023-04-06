23fd747827d22b9ac6b6f831f8abd4377364a3aa
MoAmin1995 <128181845+MoAmin1995@users.noreply.github.com>
2023-04-06 17:53:10 +0100

    added backend

--- ec2.tf
+++ ec2.tf
@@ -6,14 +6,14 @@ provider "aws" {
 
 
 # store the terraform state file in s3
-#terraform {
-  #backend "s3" {
-    #bucket  = "aosnote-terraform-state-bucket"
-    #key     = "build/terraform.tfstate"
-    #region  = "eu-west-2"
+terraform {
+  backend "s3" {
+    bucket  = "amin-terraform-state-bucket"
+    key     = "build/terraform.tfstate"
+    region  = "eu-west-2"
     #profile = "terraform-user"
-  #}
-#}
+  }
+}
 
 
 # create default vpc if one does not exit
